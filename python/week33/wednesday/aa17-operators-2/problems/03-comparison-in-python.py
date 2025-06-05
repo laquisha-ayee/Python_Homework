0ebfedb43767fb22e34800c733fe5885159ab192
laquisha-ayee <rayelaquisha@gmail.com>
2025-06-04 17:39:16 -0700

    week33wednesday

--- python/week33/wednesday/aa17-operators-2/problems/03-comparison-in-python.py
+++ python/week33/wednesday/aa17-operators-2/problems/03-comparison-in-python.py
@@ -7,11 +7,21 @@ y = '1'
 list1 = [1, 2]
 list2 = [1, 2]
 
-print(x == x)
-print(x is x)
-
 # Use the equality and identity comparison operators to compare:
 # 1. x with itself
 # 2. x with y
 # 3. list1 with list2
 
+print(x == x)
+print(x is x)
+
+
+print(x == y)      
+print(x is y)       
+
+print(list1 == list2) 
+print(list1 is list2)  
+
+
+print(not x == y) 
+print(not list1 is list2)  
